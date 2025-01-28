57ec9983eabfa14bf1530c1985b367f91500c5c5
= <>
2025-01-27 23:09:59 -0500

    update

--- src/instructions/draw.zig
+++ src/instructions/draw.zig
@@ -7,25 +7,37 @@ const memory = @import("../memory.zig");
 pub fn draw(instruction: u16, pixels: [*]u32, pixels_length: usize) !void {
     const rx: data_register.DataRegister = @enumFromInt((instruction >> 8) & 0xf);
     const ry: data_register.DataRegister = @enumFromInt((instruction >> 4) & 0xf);
-    const n = instruction & 0xf;
+    const n = (instruction & 0xf) + 1;
 
     const dx = data_register.get(rx);
     const dy = data_register.get(ry);
 
-    const i = (dy * 32) + dx;
+    const ii = (dy * 32) + dx;
 
-    std.log.info("x = {}, y = {}, i = {}, n = {}", .{ dx, dy, i, n });
+    // std.log.info("rx = {}, x = {}, ry = {}, y = {}, i = {}, n = {}", .{ rx, dx, ry, dy, i2, n });
 
-    if (i >= pixels_length) {
+    if (ii >= pixels_length) {
         return error.OutOfBoundsRender;
     }
 
-    //  for (0..n) |n| {
-    // const a = address_register.get() + ni;
-    // const data = memory.get(a);
+    for (0..n / 2) |xx| {
+        const a = address_register.get() + xx;
+        const data = memory.get(@intCast(a));
+        const dx2 = dx;
+        const dy2 = dy + xx;
+        const i = (dy2 * 32) + dx2;
+        // const ix = ((dy + (xx / 2 / 2)) * 32) + (dx + (((xx / 2) % 2) * 4));
+        pixels[i + 0] ^= if (data & 0b10000000 != 0) 0xffffffff else 0;
+        pixels[i + 1] ^= if (data & 0b01000000 != 0) 0xffffffff else 0;
+        pixels[i + 2] ^= if (data & 0b00100000 != 0) 0xffffffff else 0;
+        pixels[i + 3] ^= if (data & 0b00010000 != 0) 0xffffffff else 0;
+        pixels[i + 4] ^= if (data & 0b00001000 != 0) 0xffffffff else 0;
+        pixels[i + 5] ^= if (data & 0b00000100 != 0) 0xffffffff else 0;
+        pixels[i + 6] ^= if (data & 0b00000010 != 0) 0xffffffff else 0;
+        pixels[i + 7] ^= if (data & 0b00000001 != 0) 0xffffffff else 0;
+        // std.log.info("ix = {}", .{dy / 2});
+    }
     // pixels[i] = 0xffffffff;
-    //  }
-    pixels[i] = 0xffffffff;
 
     data_register.set(data_register.DataRegister.VF, 1);
 }
